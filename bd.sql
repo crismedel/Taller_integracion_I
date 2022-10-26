d2e98ddc7cbaf37347155715c316f07c701ea7d2
Cristóbal Medel <112023426+crismedel@users.noreply.github.com>
2022-10-26 14:13:56 -0300

    Add files via upload

---
+++ bd.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-10-2022 a las 19:09:59
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pagina_web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `ID_Publicacion` int(11) DEFAULT NULL,
+  `ID_Comentario` int(11) NOT NULL,
+  `Nombre_Usuario` char(35) DEFAULT NULL,
+  `Comentario` char(255) DEFAULT NULL,
+  `Fecha` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuenta`
+--
+
+CREATE TABLE `cuenta` (
+  `ID_Cuenta` int(11) NOT NULL,
+  `Nombre` char(45) DEFAULT NULL,
+  `Apellido` char(45) DEFAULT NULL,
+  `Fecha_Nacimiento` date DEFAULT NULL,
+  `Genero` enum('Hombre','Mujer','Otro') DEFAULT NULL,
+  `Correo` char(45) DEFAULT NULL,
+  `Contraseña` char(16) DEFAULT NULL,
+  `Num_Contacto` int(9) DEFAULT NULL,
+  `Tipo_usuario` enum('Arrendador','Estudiante') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `ID_Cuenta` int(11) DEFAULT NULL,
+  `ID_Favorito` int(11) NOT NULL,
+  `ID_Publicacion` int(11) DEFAULT NULL,
+  `Direccion` char(50) DEFAULT NULL,
+  `Num_Depto` int(4) DEFAULT NULL,
+  `Tipo_Arriendo` char(30) DEFAULT NULL,
+  `Cant_Habitaciones` int(5) DEFAULT NULL,
+  `Descripcion` char(255) DEFAULT NULL,
+  `Valor` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `publicacion`
+--
+
+CREATE TABLE `publicacion` (
+  `ID_Cuenta` int(11) DEFAULT NULL,
+  `ID_Publicacion` int(11) NOT NULL,
+  `Titulo_Arriendo` char(255) DEFAULT NULL,
+  `Direccion` char(50) DEFAULT NULL,
+  `Num_Depto` int(4) DEFAULT NULL,
+  `Tipo_Arriendo` char(30) DEFAULT NULL,
+  `Cant_Habitaciones` int(5) DEFAULT NULL,
+  `Descripcion` char(255) DEFAULT NULL,
+  `Valor` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `soporte`
+--
+
+CREATE TABLE `soporte` (
+  `ID_Soporte` int(11) NOT NULL,
+  `Correo` char(45) DEFAULT NULL,
+  `Motivo` char(255) DEFAULT NULL,
+  `Asunto` char(255) DEFAULT NULL,
+  `Mensaje` char(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`ID_Comentario`),
+  ADD KEY `ID_Publicacion` (`ID_Publicacion`);
+
+--
+-- Indices de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  ADD PRIMARY KEY (`ID_Cuenta`);
+
+--
+-- Indices de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`ID_Favorito`),
+  ADD KEY `ID_Cuenta` (`ID_Cuenta`);
+
+--
+-- Indices de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  ADD PRIMARY KEY (`ID_Publicacion`),
+  ADD KEY `ID_Cuenta` (`ID_Cuenta`);
+
+--
+-- Indices de la tabla `soporte`
+--
+ALTER TABLE `soporte`
+  ADD PRIMARY KEY (`ID_Soporte`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `ID_Comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  MODIFY `ID_Cuenta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `ID_Favorito` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  MODIFY `ID_Publicacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `soporte`
+--
+ALTER TABLE `soporte`
+  MODIFY `ID_Soporte` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`ID_Publicacion`) REFERENCES `publicacion` (`ID_Publicacion`);
+
+--
+-- Filtros para la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`ID_Favorito`) REFERENCES `publicacion` (`ID_Publicacion`),
+  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`ID_Cuenta`) REFERENCES `cuenta` (`ID_Cuenta`);
+
+--
+-- Filtros para la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  ADD CONSTRAINT `publicacion_ibfk_1` FOREIGN KEY (`ID_Cuenta`) REFERENCES `cuenta` (`ID_Cuenta`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
