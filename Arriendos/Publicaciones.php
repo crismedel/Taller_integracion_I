96bbd89cee93d98c96b94432a8a73999422629a4
Placensia <112023294+Placensia@users.noreply.github.com>
2022-11-30 00:49:42 -0300

    Add files via upload

---
+++ Arriendos/Publicaciones.php
@@ -0,0 +1,68 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Arriendos-estilos.css">
+    <title>Arriendos</title>
+</head>
+
+<body>   
+    <header>
+        <nav class="menu">
+            <a href="../Index.php">Inicio</a>
+            <a href="../Arriendos/Arriendos.php">Arriendos</a> 
+            <a href="../Soporte.php">Soporte</a>
+            <a href="subir_publicacion.php">Crear publicacion</a>
+            <a href="../perfil/miperfil.php">Mi perfil</a>
+            <form action="" method="get">
+                <input id="barra-buscador" name="busqueda" type="search" placeholder="Buscar Arriendos...">
+                <input id="boton-buscador" name="buscar" type="submit">
+            </form>            
+        </nav>
+    </header>
+
+    <main>
+        <div class="main-div">
+
+        <img src="../img/inicio.svg" alt="" style="width:300px;height:300px;"> </br>
+
+        <h3>Titulo: </h3></br></br>
+        <h3>Tipo: </h3></br></br>
+        <h3>Valor: </h3></br></br>
+           
+        </div>
+    </main>
+        <!--Solucionar el footer--
+        <footer id="contacto">
+        <div class="contenedor footer-content">
+            <div class="contact-us">
+                <h2 class="Marca">Proyecto Semestral</h2>
+             <p> Taller de integracion I</p>
+            </div>
+            <div class="social-media">
+                <a href="./" class="social-media-icon">
+                    <i class='bx bxl-whatsapp'></i>
+                </a>
+                <a href="./" class="social-media-icon">
+                    <i class='bx bxl-facebook'></i>
+                </a>
+                <a href="./" class="social-media-icon">
+                    <i class='bx bxl-instagram'></i>
+                </a>
+                <a href="./" class="social-media-icon">
+                    <i class='bx bxl-twitter'></i>
+                </a>
+                <a href="./" class="social-media-icon">
+                    <i class='bx bxl-linkedin'></i>
+                </a>
+            </div>
+        </div>
+        <div id="line"></div>
+    </footer>-->
+</html>
